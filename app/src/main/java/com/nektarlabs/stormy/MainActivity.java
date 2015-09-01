200b9123700c952a0bc5bb157c19dbc24a69dff1
vik denic <vik.denic@gmail.com>
2015-08-31 23:16:06 -0500

    Handle error on bad API response

--- app/src/main/java/com/nektarlabs/stormy/MainActivity.java
+++ app/src/main/java/com/nektarlabs/stormy/MainActivity.java
@@ -41,8 +41,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onResponse(Response response) throws IOException {
                 try {
+                    Log.v(TAG, response.body().string());
                     if (response.isSuccessful()) {
-                        Log.v(TAG, response.body().string());
+                    }
+                    else {
+                        alertUserAboutError();
                     }
                 } catch (IOException e) {
                     Log.e(TAG, "Exception caught: ", e);
@@ -51,5 +54,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Log.d(TAG, "Main UI code is running");
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 }
