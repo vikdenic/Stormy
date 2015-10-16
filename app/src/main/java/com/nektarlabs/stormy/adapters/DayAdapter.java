0d6418adb1206b635319ff3631dd10b36e93ebc3
vik denic <vik.denic@gmail.com>
2015-10-16 17:53:04 -0500

    Create ViewHolder for Adapter
    LayoutInflater provides us with view from XML, allowing us to set the ViewHolder's properties accordingly

--- app/src/main/java/com/nektarlabs/stormy/adapters/DayAdapter.java
+++ app/src/main/java/com/nektarlabs/stormy/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package com.nektarlabs.stormy.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.nektarlabs.stormy.R;
 import com.nektarlabs.stormy.weather.Day;
 
 /**
@@ -37,7 +41,30 @@ public class DayAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) { //called for initial items displayed, and new items
+        ViewHolder holder;
+
+        if (convertView == null)
+        {
+            // brand new
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
         return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
     }
 }
