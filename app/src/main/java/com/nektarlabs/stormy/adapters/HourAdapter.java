d4e8d43812fcba4acd8aa89dc11105b6ed62a3ac
vik denic <vik.denic@gmail.com>
2015-10-23 17:47:23 -0500

    Set data in Adapter via ViewHolder

--- app/src/main/java/com/nektarlabs/stormy/adapters/HourAdapter.java
+++ app/src/main/java/com/nektarlabs/stormy/adapters/HourAdapter.java
@@ -1,6 +1,7 @@
 package com.nektarlabs.stormy.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -18,40 +19,48 @@ import butterknife.ButterKnife;
  */
 public class HourAdapter extends RecyclerView.Adapter<HourAdapter.HourViewHolder> {
 
-    @Bind(R.id.timeLabel) TextView mTimeLabel;
-    @Bind(R.id.summaryLabel) TextView mSummaryLabel;
-    @Bind(R.id.temperatureLabel) TextView mTemperatureLabel;
-    @Bind(R.id.iconImageView) ImageView mIconImageView;
+    private Hour[] mHours;
+
+    public HourAdapter(Hour[] hours) {
+        mHours = hours;
+    }
 
     @Override
     public HourViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.hourly_list_item, parent, false);
+        HourViewHolder viewHolder = new HourViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(HourViewHolder holder, int position) {
-
+        holder.bindHour(mHours[position]);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mHours.length;
     }
 
     public class HourViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.timeLabel) TextView mTimeLabel;
+        @Bind(R.id.summaryLabel) TextView mSummaryLabel;
+        @Bind(R.id.temperatureLabel) TextView mTemperatureLabel;
+        @Bind(R.id.iconImageView) ImageView mIconImageView;
 
         public HourViewHolder(View itemView) {
             super(itemView);
 
             ButterKnife.bind(this, itemView);
         }
-    }
 
-    public void BindHour(Hour hour) {
-        mTimeLabel.setText(hour.getHourOfDay());
-        mSummaryLabel.setText(hour.getSummary());
-        mTemperatureLabel.setText(hour.getTemperature() + "");
-        mIconImageView.setImageResource(hour.getIconId());
+        public void bindHour(Hour hour) {
+            mTimeLabel.setText(hour.getHourOfDay());
+            mSummaryLabel.setText(hour.getSummary());
+            mTemperatureLabel.setText(hour.getTemperature() + "");
+            mIconImageView.setImageResource(hour.getIconId());
+        }
     }
 }
