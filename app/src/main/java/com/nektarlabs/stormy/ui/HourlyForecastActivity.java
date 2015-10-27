913836c113d72babc701d11f21e6231b6ab5ac60
vik denic <vik.denic@gmail.com>
2015-10-26 19:46:57 -0500

    Setup adapter methods and create LayoutManager for HourlyForecastActvity

--- app/src/main/java/com/nektarlabs/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/nektarlabs/stormy/ui/HourlyForecastActivity.java
@@ -4,24 +4,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.nektarlabs.stormy.R;
+import com.nektarlabs.stormy.adapters.HourAdapter;
 import com.nektarlabs.stormy.weather.Hour;
 
 import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class HourlyForecastActivity extends AppCompatActivity {
 
     private Hour[] mHours;
 
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
 
         Intent intent = getIntent();
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
     }
 
 }
